9475134c9d2ae159ede356cc251282de0d2deb8f
yoiner_o_ <112841034+yoiner198@users.noreply.github.com>
2022-11-07 16:18:12 -0500

    Add files via upload

--- ProyectoAula/src/vista/Vista.java
+++ ProyectoAula/src/vista/Vista.java
@@ -1,16 +1,18 @@
 package vista;
 
+import Modelo.Acita;
 import Modelo.Usuario;
 
 public class Vista {
 
     
     public static void main(String[] args) {
-        Usuario registrar = new Usuario();
+        //Usuario registrar = new Usuario();
+        Acita si = new Acita();
         
-        registrar.Registrar();
+        //registrar.Registrar();
 
-        
+        si.apartarCita();
     }
     
 }
